5d62922d4d993d22607359fa6ac52f226cd50a34
Patrick Rathmann <rathmann@projektionisten.de>
2021-04-12 15:31:45 +0200

    feat: added configs for code deployment

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,10 +2,26 @@
 STACK_NAME=aws-hello-world
 REGION=eu-central-1
 EC2_INSTANCE_TYPE=t2.micro
+PROFILE=helloworld
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --query "Account" --output text`
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name $STACK_NAME-setup \
+	--template-file bucketSetup.yml \
+	--no-fail-on-empty-changeset \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--parameter-overrides \
+	CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
 	--region $REGION \
+	--profile $CLI_PROFILE \
 	--stack-name $STACK_NAME \
 	--template-file main.yml \
 	--no-fail-on-empty-changeset \
@@ -13,9 +29,9 @@ aws cloudformation deploy \
 	--parameter-overrides \
 	EC2InstanceType=$EC2_INSTANCE_TYPE
 
-
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
 	aws cloudformation list-exports \
+		--profile $CLI_PROFILE \
 		--query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
