333fcfd101757f2d4297d8eac296a670fc948ae7
Patrick Rathmann <rathmann@projektionisten.de>
2021-04-14 07:46:15 +0200

    feat: change automatic deployment

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const { hostname } = require("os");
 const http = require("http");
 
-
-const message = "Hello World\n";
+const message = "Hello Amazon\n";
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
